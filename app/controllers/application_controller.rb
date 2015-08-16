d6b55233af3c0cbb07abd3661783e9472292cbd6
fifiteen82726 <fifiteen82726@gmail.com>
2015-08-17 01:42:04 +0800

    fix syndax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   private 
   def current_user
-  	@current_user ||= User.find(seesion[:user_id] if session[:user_id])
+  	@current_user ||= User.find(seesion[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
-  
+
 end
