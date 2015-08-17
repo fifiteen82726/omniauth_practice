b171dabb642d54264da8f07e47769547d2dc6a35
Coda <s1001514@mail.yzu.edu.tw>
2015-08-17 23:57:02 +0800

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  
-  provider :facebook, '470721796435738', '04e11041ae7e9eccb89373af37d27e05'
-  provider :github, '547848305a9d018ae3ff', '5bb39a594620653f342d9fc8921e48423caebbb4'
-  provider :twitter, 'qx5WVZeEp1z0aFMVeA8LiJq4F', 'QD5QMoJ7Ot2GTqZ6b1bJU18AAmAOcez61wPD0PDKXMrapDo8LN'
-  provider :google_oauth2, '851289300538-udcas5knf8147qq7lgs99ptdak2tkeue.apps.googleusercontent.com', '60zvnwpKdHaKBMRcKXyxN6Kg'
+  provider :facebook, 'fb-id', 'fb-pwd'
+  provider :github, 'git-id', 'git-pwd'
+  provider :twitter, 'twitter-id', 'twitter-pwd'
+  provider :google_oauth2, 'google-id', 'google-pwd''
 end
