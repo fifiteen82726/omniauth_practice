5f6550f3913c4f45f636284ae1114c2cd97e003d
fifiteen82726 <fifiteen82726@gmail.com>
2015-08-17 22:59:45 +0800

    fix client secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  
-  provider :facebook, '470721796435738', 'e65161e60fa3f914cee9c9e4b37dbe4f'
+  provider :facebook, '470721796435738', '04e11041ae7e9eccb89373af37d27e05'
   provider :github, '547848305a9d018ae3ff', '5bb39a594620653f342d9fc8921e48423caebbb4'
    provider :twitter, 'qx5WVZeEp1z0aFMVeA8LiJq4F', 'QD5QMoJ7Ot2GTqZ6b1bJU18AAmAOcez61wPD0PDKXMrapDo8LN'
 
